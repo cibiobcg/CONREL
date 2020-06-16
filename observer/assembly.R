7db5b7399c5602ce452af3b00ccc747514f36f0b
ddalfovo <davide.dalfovo@studenti.unitn.it>
2020-06-16 10:36:18 +0200

    hg38 added

--- observer/assembly.R
+++ observer/assembly.R
@@ -1,9 +1,9 @@
 if("EnsDb.Hsapiens.v75"%in%ip$Package){
   output$assembly <- renderMenu({
-    menuItem(text="Switch to GRCh38/hg38", href = "./conrelhg38",icon = icon("dna"))
+    menuItem(text="Switch to GRCh38/hg38", href = "../conrelhg38",icon = icon("dna"))
   })
 } else if("EnsDb.Hsapiens.v86"%in%ip$Package){
   output$assembly <- renderMenu({
-    menuItem("Switch to GRCh37/hg19", href = "./conrel",icon = icon("dna"))
+    menuItem("Switch to GRCh37/hg19", href = "../conrel",icon = icon("dna"))
   })
 }
